21fadeb239827b99cfa6de6d90d87f98d0852800
EmilyXu45 <emilyxu030@gmail.com>
2026-03-19 23:00:24 +0800

    Camera testing

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+# create the frontend of the app/
+import cv2
+# Allows us to use the camera and process the images
+from PIL import Image
+# Pillow: resize/format the picture inputs 
+import numpy as np
+# Process the images as numbers and feed them into the model
+
+st.title("ASL Learning App")
+st.write("Testing the camera...")
+
+img_file_buffer = st.camera_input("Take a photo of a sign:")
+# Opens the camera and takes a picture which gets stored in img_file_buffer
+if img_file_buffer is not None:
+# Checking if the user has taken a picture to be processsed.
+    img = Image.open(img_file_buffer)
+    st.image(img, caption="Sign visible", use_container_width = True)
+    st.success("Camera is working, WOOHOO!")
