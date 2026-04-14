d2d08a3d78e66bc3d1ed286dfc05a5b1f469dfe5
EmilyXu45 <emilyxu030@gmail.com>
2026-04-14 23:51:43 +0800

    Removed redundant checking code

--- check.py
+++ check.py
@@ -2,7 +2,3 @@ from google import genai
 import streamlit as st
 
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        st.write(f"Model found: **{m.name}**")
